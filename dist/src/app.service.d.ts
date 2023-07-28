6d067afc545a889ea601c0545923bc9a350ca4d7
iamsaadurrehman <saadurrehman59@gmail.com>
2023-07-29 01:33:51 +0500

    delete dist

--- dist/src/app.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Reviews } from './reviews/reviews';
-import { PageResponse } from 'types';
-export declare class AppService extends Reviews {
-    getReviews(url: string): Promise<PageResponse>;
-    processPages(): Promise<Uint8Array>;
-    pngToPdf(pngFilePath: any, pdfDoc: any): Promise<void>;
-    createPDF(): Promise<any>;
-}
--- dist/src/constant.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const CHROME_PATH = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
-export declare const AMAZON_REVIEW_PAGE: {
-    NEXT_PAGE: string;
-    ALL_REVIEWS_BUTTON: string;
-    ALL_REVIEW_PAGE: string;
-};
-export declare const REGEX_API_PATTERN: RegExp;
